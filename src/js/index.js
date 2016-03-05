1682b4d13016ca65b8f186d42625a8739a4d659a
Justin Faust <activedecay@gmail.com>
2016-03-05 07:58:39 -0700

    map state to prawpbs

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,14 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider, connect } from 'react-redux'
+import {createStore} from 'redux'
+import {Provider, connect} from 'react-redux'
+import marked from 'marked'
+import $ from 'jquery'
 
 // React component
 class Counter extends React.Component {
-  render () {
-    const { value, onIncreaseClick } = this.props
+  render() {
+    const {value, onIncreaseClick} = this.props
     return (
       <div>
         <span>{value}</span>
@@ -25,7 +27,7 @@ Counter.propTypes = {
 const increaseAction = {type: 'increase'}
 
 // Reducer
-function counter (state = {count: 0}, action) {
+function counter(state = {count: 0}, action) {
   let count = state.count
   switch (action.type) {
     case 'increase':
@@ -39,14 +41,14 @@ function counter (state = {count: 0}, action) {
 let store = createStore(counter)
 
 // Map Redux state to component props
-function mapStateToProps (state) {
+function mapStateToProps(state) {
   return {
     value: state.count
   }
 }
 
 // Map Redux actions to component props
-function mapDispatchToProps (dispatch) {
+function mapDispatchToProps(dispatch) {
   return {
     onIncreaseClick: () => dispatch(increaseAction)
   }
@@ -60,37 +62,70 @@ let App = connect(
 
 // my first component
 var CommentBox = React.createClass({
-  render: function() {
+  render: function () {
+
+    $.get(this.props.url);
+
     return (
-        <div className="commentBox">
-          Hello, world! I am a CommentBox.
-        </div>
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={data} />
+        <CommentForm />
+      </div>
     );
   }
 });
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
-        <div className="commentList">
-          Hello, world! I am a CommentList.
-        </div>
+      <div className="commentList">
+        {commentNodes}
+      </div>
     );
   }
 });
 var CommentForm = React.createClass({
-  render: function() {
+  render: function () {
     return (
-        <div className="commentForm">
-          Hello, world! I am a CommentForm.
-        </div>
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
     );
   }
 });
+var Comment = React.createClass({
+  rawMarkup: function () {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  },
+  render: function () {
+    //noinspection HtmlUnknownAttribute
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          <img /*src={this.props.author + ".png"}*/ /> {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()}/>
+      </div>
+    );
+  }
+});
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is \n\none comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
 
 ReactDOM.render(
   // <Provider store={store}>
   //   <App />
   // </Provider>,
-    <CommentBox />,
-    document.getElementById('root')
+  <CommentBox url="/api/comments" />,
+  document.getElementById('root')
 )
